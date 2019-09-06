3b5fccaf882c4ea2259af25bed294ea9bbfde61a
Udip Rai <udiprai28@gmail.com>
2019-09-06 23:02:09 +0545

    Second Commit

--- springboot.jpa_crud.repository11/src/main/java/com/manageticket/SecurityConfig.java
+++ springboot.jpa_crud.repository11/src/main/java/com/manageticket/SecurityConfig.java
@@ -1,15 +1,43 @@
 package com.manageticket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+//    @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//            .formLogin()
+//                .and()
+//            .httpBasic();
     	http.authorizeRequests().anyRequest().permitAll();
+////      .authorizeExchange()
+//      .pathMatchers("/actuator/**").permitAll()
+//      .anyExchange().authenticated()
+//      .and().build();
     }
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        return http.authorizeExchange()
+//          .pathMatchers("/actuator/**").permitAll()
+//          .anyExchange().authenticated()
+//          .and().build();
+//    }
+//    	
+//    	@Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+//        }
 }
+    
+    
--- springboot.jpa_crud.repository11/src/main/java/com/manageticket/TicketBookingManagementApplication.java
+++ springboot.jpa_crud.repository11/src/main/java/com/manageticket/TicketBookingManagementApplication.java
@@ -2,11 +2,13 @@ package com.manageticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
-        )
+//@SpringBootApplication(exclude = {
+//        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+//        )
 //@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TicketBookingManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TicketBookingManagementApplication.class, args);	
